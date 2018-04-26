8490bcaecbb69056436fcde597cf705c9549de65
antonio felices <felices.antonio@yahoo.es>
2018-04-26 21:05:20 +0200

    test widget loop

--- includes/class-widget-loops.php
+++ includes/class-widget-loops.php
@@ -42,10 +42,16 @@ class moare_loops_widget extends WP_Widget{
 
 		echo $args['before_widget'];
 
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
+		}
+
+		$cptname = $instance['cptname'];
+		$taxid = $instance['taxid'];
+
 		$argsloop = array(
-			'post_type' => 'post',
-			'cat' => array( 2, 19, 7 ),
-			'posts_per_page' => 10,
+			'post_type' => $cptname,
+			'posts_per_page' => 10
 		);
 
 		echo '<ul>';
@@ -72,6 +78,61 @@ class moare_loops_widget extends WP_Widget{
 		wp_reset_postdata();
 
 		echo $args['after_widget'];
+
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'Title', 'moarebasicp' );
+		$cptname = ! empty( $instance['cptname'] ) ? $instance['cptname'] : esc_html__( 'CPT name', 'moarebasicp' );
+		$taxid = ! empty( $instance['taxid'] ) ? $instance['taxid'] : esc_html__( 'Tax id', 'moarebasicp' );
+
+		?>
+
+		<p>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'moarebasicp' ); ?></label>
+			<br>
+			<input id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+
+		<p>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'cptname' ) ); ?>"><?php esc_attr_e( 'CPT name', 'moarebasicp' ); ?></label>
+			<br>
+			<input id="<?php echo esc_attr( $this->get_field_id( 'cptname' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'cptname' ) ); ?>" type="text" value="<?php echo esc_attr( $cptname ); ?>">
+		</p>
+
+		<p>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'taxid' ) ); ?>"><?php esc_attr_e('Tax ID', 'moarebasicp'); ?></label>
+			<br>
+			<input id="<?php echo esc_attr( $this->get_field_id( 'taxid' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'taxid' ) ); ?>" type="text" value="<?php echo esc_attr( $taxid ); ?>" />
+		</p>
+
+		<?php
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		$instance['cptname'] = ( ! empty( $new_instance['cptname'] ) ) ? strip_tags( $new_instance['cptname'] ) : '';
+		$instance['taxid'] = ( ! empty( $new_instance['taxid'] ) ) ? strip_tags( $new_instance['taxid'] ) : '';
+
+		return $instance;
 
 	}
 
